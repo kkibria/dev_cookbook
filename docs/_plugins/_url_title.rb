733690a242d67b9c51c2b1b9771ad5ec1de51a28
Khan Kibria <khan.kibria@gmail.com>
2020-02-22 12:18:11 -0800

    update

--- docs/_plugins/_url_title.rb
+++ docs/_plugins/_url_title.rb
@@ -2,14 +2,14 @@ require 'liquid'
 
 # Capitalize all words of the input
 module Jekyll
-    module UrlTitle
-        def url_title(words)
-            return words.split('/').last.split('.').first.capitalize.split(/[-,_]/).join(' ')
-            # a = words.split('/').last.split('.').first.split(/[-,_]/)
-            # b = a.map(&:capitalize).join(' ')
-            # return b
-        end
+  module UrlTitle
+    def url_title(words)
+      return words.split('/').last.split('.').first.capitalize.split(/[-,_]/).join(' ')
+      # a = words.split('/').last.split('.').first.split(/[-,_]/)
+      # b = a.map(&:capitalize).join(' ')
+      # return b
     end
+  end
 end
   
 Liquid::Template.register_filter(Jekyll::UrlTitle)
